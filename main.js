c0f9ac15ec4c161222cd83edd66beb93d4d638eb
Noof <noof.mohammed998@gmail.com>
2021-05-14 16:28:29 +0300

    clean code

--- main.js
+++ main.js
@@ -3,53 +3,37 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-// app.use(express.json());
 app.use(express.json());
 const articlesRouter = express.Router();
 
 const articles = [
-    {
+  {
     id: 1,
-    title: 'How I learn coding?',
-    description:
-    'Lorem, Quam, mollitia.',
-    author: 'Jouza',
-    },
-    {
+    title: "How I learn coding?",
+    description: "Lorem, Quam, mollitia.",
+    author: "Jouza",
+  },
+  {
     id: 2,
-    title: 'Coding Best Practices',
-    description:
-    'Lorem, ipsum dolor sit, Quam, mollitia.',
-    author: 'Besslan',
-    },
-    {
+    title: "Coding Best Practices",
+    description: "Lorem, ipsum dolor sit, Quam, mollitia.",
+    author: "Besslan",
+  },
+  {
     id: 3,
-    title: 'Debugging',
-    description:
-    'Lorem, Quam, mollitia.',
-    author: 'Jouza',
-    },
-    ];
+    title: "Debugging",
+    description: "Lorem, Quam, mollitia.",
+    author: "Jouza",
+  },
+];
 
-    // const getAllArticles = (req,res) =>{
-    // //    console.log(articles);
-    //    res.status(200)
-    //    res.json(articles) 
-    // }
-    // getAllArticles()
-    
-    articlesRouter.get("/articles",(req,res) =>{
-        //    console.log(articles);
-           res.status(200)
-           res.json(articles) 
-        })
+articlesRouter.get("/articles", (req, res) => {
+  res.status(200);
+  res.json(articles);
+});
 
-        app.use(articlesRouter)
+app.use(articlesRouter);
 
-
-        app.listen(port,() =>{
-            console.log(`The server is start ${port}`)
-        })
-    // app.get("/articles", (res,req)=>{
-
-    // })
+app.listen(port, () => {
+  console.log(`The server is start ${port}`);
+});
