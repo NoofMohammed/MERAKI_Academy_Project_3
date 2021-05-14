ce20b336f9f9758fee03e7825da7fb59bef3c966
Noof <noof.mohammed998@gmail.com>
2021-05-14 18:45:00 +0300

    getArticlesByAuthor

--- main.js
+++ main.js
@@ -6,9 +6,6 @@ const port = 5000;
 app.use(express.json());
 const articlesRouter = express.Router();
 
-
-
-
 const articles = [
   {
     id: 1,
@@ -35,24 +32,24 @@ articlesRouter.get("/articles", (req, res) => {
   res.json(articles);
 });
 
+articlesRouter.get("/articles/search_1", (req, res) => {
+  const author = req.query.author;
+  console.log(author, 9999);
+  const article = articles.filter((elem) => {
+    return elem.author === author;
+  });
+  res.status(200);
+  res.json(article);
+});
 
-
-
-articlesRouter.get("/articles/:id",(req,res) => {
-    const id = req.params.id
-    const artic = articles.find((elem) =>{
-       return elem.id === Number(id) 
-    })
-    res.status(200)
-    res.json(artic)
-    
-})
-
-
-
-
-
-
+articlesRouter.get("/articles/:id", (req, res) => {
+  const id = req.params.id;
+  const artic = articles.find((elem) => {
+    return elem.id === Number(id);
+  });
+  res.status(200);
+  res.json(artic);
+});
 
 app.use(articlesRouter);
 app.listen(port, () => {
