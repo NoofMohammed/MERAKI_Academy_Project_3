4033878cc06e31bdeca3dc51c4d930f635fe672c
Noof <noof.mohammed998@gmail.com>
2021-05-18 08:40:53 +0300

    fixes

--- main.js
+++ main.js
@@ -6,12 +6,10 @@ const app = express();
 const { users, articlesSch } = require("./schema");
 const port = 5000;
 
-// const { uuid } = require("uuidv4");
-
 app.use(express.json());
 const articlesRouter = express.Router();
 const usersRouter = express.Router();
-
+// getAllArticles
 articlesRouter.get("/articles", (req, res) => {
   articlesSch
     .find({})
@@ -22,7 +20,7 @@ articlesRouter.get("/articles", (req, res) => {
       res.send(err);
     });
 });
-
+// getArticlesByAuthor
 articlesRouter.get("/articles/search_1", async (req, res) => {
   const firstName = req.query.author;
   const author = await users.findOne({ firstName });
@@ -36,7 +34,7 @@ articlesRouter.get("/articles/search_1", async (req, res) => {
       res.send(err);
     });
 });
-
+// getAnArticleById
 articlesRouter.get("/articles/search_2/:id", (req, res) => {
   const _id = req.params.id;
 
@@ -67,11 +65,16 @@ articlesRouter.post("/articles", (req, res) => {
     });
 });
 
+// updateAnArticleById
 articlesRouter.put("/articles/:id", (req, res) => {
   const _id = req.params.id;
   const { title, description, authorId } = req.body;
   articlesSch
-    .findOneAndUpdate({ title, description, author: authorId })
+    .findOneAndUpdate(
+      { _id },
+      { title, description, author: authorId },
+      { new: true }
+    )
     .then((result) => {
       res.send(result);
     })
@@ -80,7 +83,8 @@ articlesRouter.put("/articles/:id", (req, res) => {
     });
 });
 
-articlesRouter.delete("/articles/:id", (req, res) => {
+//deleteArticleById
+articlesRouter.delete("/articles/:id", async (req, res) => {
   const _id = req.params.id;
   articlesSch
     .findOneAndRemove({ _id })
@@ -92,8 +96,12 @@ articlesRouter.delete("/articles/:id", (req, res) => {
     });
 });
 
-articlesRouter.delete("/articles", (req, res) => {
-  const _id = req.params.id;
+// deleteArticlesByAuthor
+articlesRouter.delete("/articles", async (req, res) => {
+  const firstName = req.body.author;
+  const author = await users.findOne({ firstName });
+
+  const _id = author.id;
   articlesSch
     .findOneAndRemove({ _id })
     .then((result) => {
